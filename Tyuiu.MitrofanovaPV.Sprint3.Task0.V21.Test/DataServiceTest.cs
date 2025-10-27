23556fb8e74d7d57bf9e7d605a13ad0e4821eff8
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-27 21:38:44 +0500

    Add project files.

---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task0.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task0.V21.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint3.Task0.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetSumSeries()
+        {
+            DataService ds = new DataService();
+            double value = 1;
+            int startValue = 1;
+            int stopValue = 2;
+            double res = ds.GetSumSeries(value, startValue, stopValue);
+            double wait = 2;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task0.V21/Program.cs
@@ -0,0 +1,14 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task0.V21.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        double value = 1.5;
+        int startValue = 1;
+        int stopValue = 13;
+        Console.WriteLine("Сумма ряда =" + ds.GetSumSeries(value, startValue, stopValue));
+        Console.ReadKey();
+    }
+}
+    
