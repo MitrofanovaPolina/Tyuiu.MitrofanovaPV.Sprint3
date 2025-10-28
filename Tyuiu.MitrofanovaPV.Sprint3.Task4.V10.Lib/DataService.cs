10dceebad59b23ca97322bee9b16bf196201f09d
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-28 19:46:27 +0500

    Task4

--- Tyuiu.MitrofanovaPV.Sprint3.Task4.V10.Lib/DataService.cs
+++ Tyuiu.MitrofanovaPV.Sprint3.Task4.V10.Lib/DataService.cs
@@ -1,14 +1,14 @@
 ﻿using tyuiu.cources.programming.interfaces.Sprint3;
 namespace Tyuiu.MitrofanovaPV.Sprint3.Task4.V10.Lib
 {
-    public class DataService : ISprint3Task4V21
+    public class DataService : ISprint3Task4V10
     {
         public double Calculate(int startValue, int stopValue)
         {
             double res = 0;
             for (int x = startValue; x <= stopValue; x++)
             {
-                if (x==0)
+                if (x == 0)
                 {
                     break;
                 }
