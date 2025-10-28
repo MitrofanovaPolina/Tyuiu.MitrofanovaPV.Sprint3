394e743d888624bc809477cf596c50057c26a044
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-28 19:18:04 +0500

    Task3

---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task3.V21.Test/DataServiceTest.cs
@@ -0,0 +1,19 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task3.V21.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint3.Task3.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            string value = "yftdtdy656";
+            char item = 'e';
+            string res = ds.ReplaceNumOnChar(value, item);
+            string wait = "gft6d6tdt6";
+            Assert.AreEqual(res,wait);
+
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task3.V21/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task3.V21.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        string value = "f3g5ht g4j 34kg4";
+        char item = 'e';
+        
+        Console.WriteLine(ds.ReplaceNumOnChar(value, item));
+        Console.ReadKey();
+    }
+}
