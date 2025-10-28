2beb6929b96684b1cf83bef120a259a7137e5f40
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-28 21:31:34 +0500

    Task5

---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task5.V18.Test/DataServiceTest.cs
@@ -0,0 +1,21 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task5.V18.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint3.Task5.V18.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int x = 1;
+            int startValue1 = 1;
+            int startValue2 = 1;
+            int stopValue1 = 1;
+            int stopValue2 = 1;
+            double res = ds.GetSumSumSeries(x,startValue1, startValue2, stopValue1, stopValue2);
+            double wait = 1;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task5.V18/Program.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task5.V18.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int x = 5;
+        int startValue1 =1;
+        int startValue2 = 1;
+        int stopValue1 = 3;
+        int stopValue2 = 11;
+
+        Console.WriteLine(ds.GetSumSumSeries(x,startValue1,startValue2,stopValue1,stopValue2));
+        Console.ReadKey();
+    }
+}
+
