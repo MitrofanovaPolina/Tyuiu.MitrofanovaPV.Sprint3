df944af79d06075cedc3f1b7cab416346c545e50
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-28 22:48:45 +0500

    Task7

---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task7.V21.Test/DataServiceTest.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task7.V21.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint3.Task7.V21.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            int len = stopValue - startValue + 1;
+            double[] valueWaitArray;
+            valueWaitArray = new double[len];
+            valueWaitArray[0] = 1;
+            valueWaitArray[1] = 2;
+            valueWaitArray[2] = 3;
+            valueWaitArray[3] = 4;
+            valueWaitArray[4] = 5;
+            valueWaitArray[5] = 6;
+            valueWaitArray[6] = 6;
+            valueWaitArray[7] = 6;
+            valueWaitArray[8] = 6;
+            valueWaitArray[9] = 6;
+            valueWaitArray[10] = 6;
+            double[] res;
+            res = new double[len];
+            res = ds.GetMassFunction(startValue,stopValue);
+            CollectionAssert.AreEqual(res, valueWaitArray);
+
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task7.V21/Program.cs
@@ -0,0 +1,22 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task7.V21.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int startValue = -5;
+        int stopValue = 5;
+        int len = ds.GetMassFunction(startValue, stopValue).Length;
+        double[] valueArray;
+        valueArray = new double[len];
+        valueArray = ds.GetMassFunction(startValue, stopValue);
+        Console.WriteLine("+-------------+-------------+");
+        Console.WriteLine("|     x       |      f(x)    ");
+        Console.WriteLine("+-------------+-------------+");
+        for (int i = 0;  i <= len-1; i++)
+        {
+            Console.WriteLine("| {0,5:}  |   {1,5:f2}    |", startValue, valueArray[i]);
+        }
+        Console.ReadKey();
+    }
+}
