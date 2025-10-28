dfc43865103a449e3f9de989c70eb6208b05ff05
MitrofanovaPolina <polina_mitrofanova08@mail.ru>
2025-10-28 19:43:22 +0500

    Task4

---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task4.V10.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task4.V10.Lib;
+namespace Tyuiu.MitrofanovaPV.Sprint3.Task4.V10.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+            int startValue = -5;
+            int stopValue = 5;
+            double res = ds.Calculate(startValue, stopValue);
+            double wait = 1;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.MitrofanovaPV.Sprint3.Task4.V10/Program.cs
@@ -0,0 +1,13 @@
+﻿using Tyuiu.MitrofanovaPV.Sprint3.Task4.V10.Lib;
+class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+        int startValue = -5;
+        int stopValue = 5;
+
+        Console.WriteLine(ds.Calculate(startValue, stopValue));
+        Console.ReadKey();
+    }
+}
